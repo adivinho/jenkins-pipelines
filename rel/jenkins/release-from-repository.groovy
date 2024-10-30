4998ad15af4d4ff290db79491ae6724369a3319a
Vadim Yalovets <vadim.yalovets@percona.com>
2024-10-30 19:21:54 +0200

    move release jobs to rel.cd

--- rel/jenkins/release-from-repository.groovy
+++ rel/jenkins/release-from-repository.groovy
@@ -285,6 +285,12 @@ ENDSSH
                                           fi
                                        done
                                    fi
+                                   # -------------------------------------> sync packages
+                                   cd \${RELEASEDIR}/..
+                                   ln -s \${RELEASE} LATEST
+                                   cd /srv/UPLOAD/${PATH_TO_BUILD}/.tmp
+                                   echo "rsync -avt -e "ssh -p 2222" --bwlimit=50000 --exclude="*yassl*" --progress \${PRODUCT} jenkins-deploy.jenkins-deploy.web.r.int.percona.com:/data/downloads/"
+                                   #rm -fr /srv/UPLOAD/\${PATH_TO_BUILD}/.tmp
                                fi
 ENDSSH
                        else
