4cf3b703ff81f022987049cd87b2d912b5083a51
Vadim Yalovets <vadim.yalovets@percona.com>
2024-10-30 17:40:01 +0200

    move release jobs to rel.cd

--- rel/jenkins/release-from-repository.groovy
+++ rel/jenkins/release-from-repository.groovy
@@ -195,53 +195,59 @@ ENDSSH
             steps {
                 withCredentials([sshUserPrivateKey(credentialsId: 'repo.ci.percona.com', keyFileVariable: 'KEY_PATH', usernameVariable: 'USER')]) {
                     sh """
-                        ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${KEY_PATH} ${USER}@repo.ci.percona.com << 'ENDSSH'
-                            set -o errexit
-                            set -o xtrace
-                            LCREPOSITORY=\$(echo "${REPOSITORY}" | tr '[:upper:]' '[:lower:]')
-                            cd /srv/repo-copy
-                            RSYNC_TRANSFER_OPTS=" -avt --delete --delete-excluded --delete-after --progress"
-                            if [[ ${REVERSE} = true ]]; then
-                                rsync \${RSYNC_TRANSFER_OPTS} 10.30.9.32:/www/repo.percona.com/htdocs/\${LCREPOSITORY}/* /srv/repo-copy/\${LCREPOSITORY}/
-                            else
-                                rsync \${RSYNC_TRANSFER_OPTS} --exclude=*.sh --exclude=*.bak /srv/repo-copy/\${LCREPOSITORY}/* 10.30.9.32:/www/repo.percona.com/htdocs/\${LCREPOSITORY}/
-                                rsync \${RSYNC_TRANSFER_OPTS} --exclude=*.sh --exclude=*.bak /srv/repo-copy/version 10.30.9.32:/www/repo.percona.com/htdocs/
-                            fi
+                        if [ ${SKIP_REPO_SYNC} = false ]; then
+                            ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${KEY_PATH} ${USER}@repo.ci.percona.com << 'ENDSSH'
+                                set -o errexit
+                                set -o xtrace
+                                LCREPOSITORY=\$(echo "${REPOSITORY}" | tr '[:upper:]' '[:lower:]')
+                                cd /srv/repo-copy
+                                RSYNC_TRANSFER_OPTS=" -avt --delete --delete-excluded --delete-after --progress"
+                                if [[ ${REVERSE} = true ]]; then
+                                    rsync \${RSYNC_TRANSFER_OPTS} 10.30.9.32:/www/repo.percona.com/htdocs/\${LCREPOSITORY}/* /srv/repo-copy/\${LCREPOSITORY}/
+                                else
+                                    rsync \${RSYNC_TRANSFER_OPTS} --exclude=*.sh --exclude=*.bak /srv/repo-copy/\${LCREPOSITORY}/* 10.30.9.32:/www/repo.percona.com/htdocs/\${LCREPOSITORY}/
+                                    rsync \${RSYNC_TRANSFER_OPTS} --exclude=*.sh --exclude=*.bak /srv/repo-copy/version 10.30.9.32:/www/repo.percona.com/htdocs/
+                                fi
 ENDSSH
+                        else
+                            echo "The step is skipped."
+                        fi
                     """
                 }
             }
         }
         stage('Sync packages to production download') {
             steps {
-               sh """
-                   if [ ${COMPONENT} = RELEASE ]; then
-                         set -e
-                         cd /srv/UPLOAD/${PATH_TO_BUILD}/
-                         PRODUCT=\$(echo ${PATH_TO_BUILD} | awk -F '/' '{print \$3}')
-                         RELEASE=\$(echo ${PATH_TO_BUILD} | awk -F '/' '{print \$4}')
-                         REVISION=\$(echo ${PATH_TO_BUILD} | awk -F '/' '{print \$6}')
-                         RELEASEDIR="/srv/UPLOAD/${PATH_TO_BUILD}/.tmp/\${PRODUCT}/\${RELEASE}"
-                         rm -fr /srv/UPLOAD/${PATH_TO_BUILD}/.tmp
-                         mkdir -p \${RELEASEDIR}
-                         cp -av ./* \${RELEASEDIR}
-                         # -------------------------------------> create RedHat tar bundles
-                         cd \${RELEASEDIR}/binary/redhat
-                         for _dist in *; do
-                             cd \${_dist}
-                             for _arch in *; do
-                                 cd \${_arch}
-                                 # don't create bundle if there's only 1 package inside directory
-                                 NUM_PACKAGES=\$(find . -maxdepth 1 -type f -name '*.rpm'|wc -l)
-                                 if [ \${NUM_PACKAGES} -gt 1 ]; then
-                                     tar --owner=0 --group=0 -cf \${RELEASE}-r\${REVISION}-el\${_dist}-\${_arch}-bundle.tar  *.rpm
-                                 fi
-                                 cd ..
-                             done
-                             cd ..
-                         done
-                   fi
-               """
+               withCredentials([sshUserPrivateKey(credentialsId: 'repo.ci.percona.com', keyFileVariable: 'KEY_PATH', usernameVariable: 'USER')]) {
+                   sh """
+                       if [ ${COMPONENT} = RELEASE ]; then
+                           set -e
+                           cd /srv/UPLOAD/${PATH_TO_BUILD}/
+                           PRODUCT=\$(echo ${PATH_TO_BUILD} | awk -F '/' '{print \$3}')
+                           RELEASE=\$(echo ${PATH_TO_BUILD} | awk -F '/' '{print \$4}')
+                           REVISION=\$(echo ${PATH_TO_BUILD} | awk -F '/' '{print \$6}')
+                           RELEASEDIR="/srv/UPLOAD/${PATH_TO_BUILD}/.tmp/\${PRODUCT}/\${RELEASE}"
+                           rm -fr /srv/UPLOAD/${PATH_TO_BUILD}/.tmp
+                           mkdir -p \${RELEASEDIR}
+                           cp -av ./* \${RELEASEDIR}
+                           # -------------------------------------> create RedHat tar bundles
+                           cd \${RELEASEDIR}/binary/redhat
+                           for _dist in *; do
+                               cd \${_dist}
+                               for _arch in *; do
+                                   cd \${_arch}
+                                   # don't create bundle if there's only 1 package inside directory
+                                   NUM_PACKAGES=\$(find . -maxdepth 1 -type f -name '*.rpm'|wc -l)
+                                   if [ \${NUM_PACKAGES} -gt 1 ]; then
+                                       tar --owner=0 --group=0 -cf \${RELEASE}-r\${REVISION}-el\${_dist}-\${_arch}-bundle.tar  *.rpm
+                                   fi
+                                   cd ..
+                               done
+                               cd ..
+                           done
+                       fi
+                   """
+                }
             }
         }
         stage('Refresh downloads area') {
