602833c76cd511134ba6bc9d943b2201e70fc20c
Vadim Yalovets <vadim.yalovets@percona.com>
2026-09-07 18:07:20 +0300

    PXC-5223 Packaging tasks for release - PXC 9.7.1

--- ps/jenkins/percona-server-for-mysql-8.0.groovy
+++ ps/jenkins/percona-server-for-mysql-8.0.groovy
@@ -58,6 +58,8 @@ void buildStage(String DOCKER_OS, String STAGE_PARAM) {
               wget \$(echo ${GIT_REPO} | sed -re 's|github.com|raw.githubusercontent.com|; s|\\.git\$||')/${BRANCH}/build-ps/percona-server-8.0_builder.sh -O ps_builder.sh || curl \$(echo ${GIT_REPO} | sed -re 's|github.com|raw.githubusercontent.com|; s|\\.git\$||')/${BRANCH}/build-ps/percona-server-8.0_builder.sh -o ps_builder.sh
               sed -i '0,/cd \${WORKDIR}/{s|cd \${WORKDIR}/percona-server|cd \${WORKDIR}/percona-server\\n    curl -L "https://github.com/percona/percona-server/compare/release-8.0.34-26...satya-bodapati:percona-server:PS-9107-8.0.34.patch" -o PS-9107-8.0.34.patch\\n    git apply --verbose PS-9107-8.0.34.patch|}' ps_builder.sh
           fi
+          sed -i 's|^    wget https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.gz|    #wget https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.gz|' ps_builder.sh
+          sed -i 's|^    #wget http://jenkins.percona.com/downloads/boost/\${BOOST_PACKAGE_NAME}.tar.gz|    wget http://jenkins.percona.com/downloads/boost/\${BOOST_PACKAGE_NAME}.tar.gz|' ps_builder.sh
           ls -la
           export build_dir=\$(pwd -P)
           if [ "$DOCKER_OS" = "none" ]; then
